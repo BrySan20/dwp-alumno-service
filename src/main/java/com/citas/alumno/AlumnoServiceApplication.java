6f01b8e6d2f38348980622b04c3ebd45728a71bb
BrySan20 <bryansanchez1680@gmail.com>
2025-08-20 14:51:23 -0600

    Modificacion de properties 5

--- src/main/java/com/citas/alumno/AlumnoServiceApplication.java
+++ src/main/java/com/citas/alumno/AlumnoServiceApplication.java
@@ -1,5 +1,7 @@
 package com.citas.alumno;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,4 +10,13 @@ public class AlumnoServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AlumnoServiceApplication.class, args);
     }
+
+    @PostConstruct
+    public void printEnv() {
+        System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+        System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+        System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+        System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+    }
+
 }
